fa413a77ab75934f7a74f14de95fbd9200c26205
Bauer Kirill <bk.evg@mail.ru>
2025-03-05 20:55:25 +0700

    fix

--- media_sockets/main.py
+++ media_sockets/main.py
@@ -62,12 +62,13 @@ async def main():
                 data = conn.recv(160)
                 if not data:
                     break
-                if speech_processor.locate_speech(data):
-                    response_pcm = await speech_processor.handle_audio(data)
-                    # if response_pcm:
-                    #     frame = AudioConverter.create_audio_frame(response_pcm)
-                    #     conn.send(frame)
-                    conn.send(data)
+                # if speech_processor.locate_speech(data):
+                #     response_pcm = await speech_processor.handle_audio(data)
+                #     # if response_pcm:
+                #     #     frame = AudioConverter.create_audio_frame(response_pcm)
+                #     #     conn.send(frame)
+                #     conn.send(data)
+                print(data)
 
 
 if __name__ == "__main__":
