4746a742fc31ba18820983969896ca94cfc561f8
Bauer Kirill <bk.evg@mail.ru>
2025-03-12 17:42:50 +0700

    fix

--- media_sockets/main.py
+++ media_sockets/main.py
@@ -80,7 +80,7 @@ async def realtime_listener(websocket, writer):
                     ))
 
                     await writer.drain()
-                    await asyncio.sleep(0.002)
+                    await asyncio.sleep(0.01)
 
         elif event_type == "response.text.delta":
             # Если нужен текст - обрабатываем.
