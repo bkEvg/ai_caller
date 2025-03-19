b6f22d17c2bbc66aa5fbd32b1859934601e7b08d
Bauer Kirill <bk.evg@mail.ru>
2025-03-19 19:25:37 +0700

    remote timeout from recv func

--- media_sockets/main.py
+++ media_sockets/main.py
@@ -78,8 +78,7 @@ async def realtime_listener(websocket, writer):
     """
     while True:
         # Ждём следующего server->client сообщения от Realtime API
-        # msg = await websocket.recv()
-        msg = await asyncio.wait_for(websocket.recv(), timeout=10)
+        msg = await websocket.recv()
         event = json.loads(msg)
         event_type = event.get("type", "")
         logger.info(f"Получено сообщение от WebSocket {event_type} ")
