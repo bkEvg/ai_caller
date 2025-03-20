ee123d38c5c49cd265f22ff19a41449cfbe4ae33
Bauer Kirill <bk.evg@mail.ru>
2025-03-20 13:50:02 +0700

    return sleep

--- media_sockets/main.py
+++ media_sockets/main.py
@@ -99,7 +99,7 @@ async def realtime_listener(websocket, writer):
                     ))
 
                     await writer.drain()
-                    # await asyncio.sleep(0.01)
+                    await asyncio.sleep(0.01)
 
         elif event_type == "response.text.delta":
             # Если нужен текст - обрабатываем.
